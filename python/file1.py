82e58adbef54d0faf2f03283ed277a66beee5463
kavitha <kavithabattula16@gmail.com>
2026-07-20 15:29:20 +0530

    add the all files

---
+++ python/file1.py
@@ -0,0 +1,6 @@
+file = open("sample.txt", "w") 
+file.write("Hello World\n")
+file.write("Hello Mahesh")
+
+file.close()
+
---
+++ python/hello.py
@@ -0,0 +1,5 @@
+a = "Hello WOrld"
+print(len(a))
+print("lower:", a.lower())
+print("upper:", a.upper())
+
