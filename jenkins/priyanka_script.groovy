3bb0872c85055fe39e13b1c60fa726f06c8f3966
Priyanka Sengottuvel <psengottuvel@localhost.localdomain>
2026-07-22 14:18:56 +0530

    Modified the script as per the pipeline syntax

--- jenkins/priyanka_script.groovy
+++ jenkins/priyanka_script.groovy
@@ -1,5 +1,37 @@
+pipeline{
 
-#!/usr/bin/python3
+agent any
 
-print("Welcome to Jenkins Job Setup")
+stages{
 
+stage('Integration'){
+
+steps{
+       sh '''
+       
+       echo "This is an integration stage"
+       
+       '''
+}
+}
+stage('Test'){
+
+steps{
+     sh '''
+     echo "This is an Testing stage"
+    '''
+}
+}
+stage('Deployment'){
+
+steps{
+
+sh '''
+
+echo "This is a deployment phase"
+
+'''
+}
+}
+}
+}
