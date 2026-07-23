41f3b7d41e3a6e7a70971d09daf9412d16bea8d8
Priyanka Sengottuvel <psengottuvel@localhost.localdomain>
2026-07-23 21:58:08 +0530

    Script to take backup

---
+++ jenkins/backup.groovy
@@ -0,0 +1,19 @@
+pipeline{
+
+agent any
+
+stages{
+
+stage('Run Command'){
+
+steps{  
+    sh '''
+    
+    tar -czvf backup_$(date +%d-%m-%Y).tar.gz /etc /var/www /home'
+    
+    echo "The backup is successful"
+    '''
+}
+}
+}
+}
