734ce4b822b2903b19a9ee23b9803694337fd87d
Priyanka Sengottuvel <psengottuvel@localhost.localdomain>
2026-07-31 22:17:56 +0530

    Fixing the username script groovy

--- jenkins/username.groovy
+++ jenkins/username.groovy
@@ -4,13 +4,15 @@ agent { label 'Jenkins_Child'}
 
 environment {SCRIPT_PATH='python/user_input.py'}
 
+parameters {string(name:'USER_NAME',defaultvalue:'Guest', description: 'Enter your name:')}
+
 stages{
 
 stage('Run Python Script'){
 
 steps{
 
-  sh 'python3 ${SCRIPT_PATH}'
+  sh 'python3 ${SCRIPT_PATH} ${params.USER_NAME}'
 }
 }
 }
