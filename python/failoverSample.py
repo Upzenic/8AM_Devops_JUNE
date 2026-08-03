31a8355a64670fdfe0736c14b51154800ea1aad7
Priyanka Sengottuvel <psengottuvel@localhost.localdomain>
2026-08-03 13:08:59 +0530

    Fixed script

--- python/failoverSample.py
+++ python/failoverSample.py
@@ -4,12 +4,12 @@
 def addNumbers(num1, num2):
  type1 = type(num1)
  type2 = type(num2)
-    try:
+ try:
         if type1 == type2:
             return num1+num2
         else:
             raise TypeError(f"Type Mismatch:{type1} does not match with {type2}")
-    except TypeError as e:
+ except TypeError as e:
         print(f"Error:{e}")
         return e
 
